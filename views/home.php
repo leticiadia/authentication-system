ff4a0fc10bf082fe8f5abed577cba9b2999a2fff
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-10 18:49:42 -0300

    feat: create home page

---
+++ views/home.php
@@ -0,0 +1,26 @@
+<?php include 'start.html'; ?>
+
+<div class="login">
+    <h2>Have an account?</h2>
+
+    <form action="{{ login }}" method="post">
+        <?php if (isset($_GET['message'])) : ?>
+            <p class="error-message"><?= $_GET['message']; ?></p>
+        <?php endif; ?>
+
+        <div class="login-content">
+            <div class="input-username">
+                <input type="text" name="username" id="username" placeholder="Username">
+            </div>
+
+            <div class="input-password">
+                <input type="password" name="password" id="password" placeholder="Password">
+            </div>
+
+            <div class="button">
+                <button type="submit">Log in</button>
+            </div>
+        </div>
+    </form>
+</div>
+<?php include 'end.html' ?>
