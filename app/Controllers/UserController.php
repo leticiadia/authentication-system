fb252ecfbc6b0d35d0f913c1ba0b4c00948b3ade
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-13 17:06:50 -0300

    feat: store() and getUsers methods created

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Project\Authentication\Controllers;
+
+use Exception;
+use PDO;
+use Throwable;
+use Project\Authentication\Models\User;
+
+require_once __DIR__ . '/../../autoload.php';
+
+class UserController extends User
+{
+
+    private  $databaseHost;
+
+    public function __construct($database, $host, $username, $password)
+    {
+        try {
+            $this->databaseHost = new PDO(sprintf('mysql:host=%s;dbname=%s', $host, $database), $username, $password);
+        } catch (Throwable $error) {
+            $error->getMessage();
+        }
+    }
+
+    public function store($username, $password)
+    {
+        $password = password_hash($password, PASSWORD_BCRYPT);
+
+        $statement = $this->databaseHost->prepare('INSERT INTO ' . $this->table . ' (username, password) VALUES (:username, :password)');
+
+        $statement->execute([':username' => $username, ':password' => $password]);
+
+        try {
+            if (!$statement) {
+                throw new Exception('Invalid statement');
+            }
+        } catch (Throwable $error) {
+            $error->getMessage();
+        }
+    }
+
+    public function getUsers($username, $password)
+    {
+        $statement = $this->databaseHost->prepare('SELECT * FROM ' . $this->table . ' WHERE username = :username');
+        if (!$statement) throw new Exception('Invalid statement');
+
+        $result = $statement->execute([':username' => $username]);
+        if (!$result) throw new Exception(implode(' ', $statement->errorInfo()));
+
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
+        if (!is_array($row)) return false;
+
+        return password_verify($password, $row['password']);
+    }
+}
